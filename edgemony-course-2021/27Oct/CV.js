cc413358dc8dcc443c5075992059f17d97d5867e
unknown <tirritoroberto@gmail.com>
2021-10-28 16:07:46 +0200

    prove colore-somma

--- edgemony-course-2021/27Oct/CV.js
+++ edgemony-course-2021/27Oct/CV.js
@@ -5,3 +5,26 @@ darkModeBtn.addEventListener(
  document.body.classList.toggle("darkMode");
 }
 );
+
+document.addEventListener("click", (event) => {
+    const somma = document.querySelector(".somma");
+    let numberChoise = prompt ("inserisci numeri");
+    const numbers = numberChoise.split(",")  
+
+    const parsedNumbers =[]
+    const sum = (numOne, numTwo) => numOne + numTwo;
+
+    console.log();
+}
+);
+
+// function colorCh(){
+// const r = Math.round(Math.random()*256);
+// const g = Math.round(Math.random()*256);
+// const b = Math.round(Math.random()*256);
+
+// colore_rgb = "rgb("+r+","+g+","+b+",)";
+// const colorCh = document.querySelector (".circle")
+// classList.toggle.colorCh
+// }
+// setInterval(colorCh,5000);
